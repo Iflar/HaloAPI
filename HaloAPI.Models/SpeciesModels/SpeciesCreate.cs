c4dc24f3e935a7cb5db698e506d0a42728687bf5
Peter-Nikolov1 <nikolov_peter@yahoo.com>
2021-12-09 20:36:41 -0500

    Added Create, Edit, Detail, ListItem

---
+++ HaloAPI.Models/SpeciesModels/SpeciesCreate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaloAPI.Models.SpeciesModels
+{
+    public class SpeciesCreate
+    {
+        
+        public int Lifespan { get; set; }
+        
+        [MinLength(2, ErrorMessage ="Please enter a longer Origin name.")]
+        [MaxLength(100, ErrorMessage ="Please enter a shorter Origin name.")]
+        public string Origin { get; set; }
+        
+        [MinLength(2, ErrorMessage ="Please enter a longer Species name.")]
+        [MaxLength(800, ErrorMessage ="Please enter a shorter Species name.")]
+        public string SpeciesName { get; set; }
+
+        public double Height { get; set; }
+    }
+}
