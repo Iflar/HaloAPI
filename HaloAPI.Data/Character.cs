1cd82d7f1068ce4ffbd76f98c5c07371c58fede9
iflar <classicsience@gmail.com>
2021-12-09 19:02:10 -0500

    added Character data layer

---
+++ HaloAPI.Data/Character.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaloAPI.Data
+{
+    public class Character
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [ForeignKey(nameof(Species))]
+        public int SpeciesId { get; set; }
+
+        [ForeignKey(nameof(Faction))]
+        public int FactionId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Renown { get; set; }
+    }
+}
