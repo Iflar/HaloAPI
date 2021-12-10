fea366b058dee0862ee70d05f8ec797c5650d385
Peter-Nikolov1 <nikolov_peter@yahoo.com>
2021-12-09 21:30:41 -0500

    Removed Identity Models

---
+++ HaloAPI.Services/SpeciesService.cs
@@ -0,0 +1,23 @@
+﻿using HaloAPI.Data;
+using HaloAPI.Models.SpeciesModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaloAPI.Services
+{
+    public class SpeciesService
+    {
+        public bool CreateSpecies(SpeciesCreate)
+        {
+            var entity = 
+                new Species()
+                {
+
+                }
+        }
+
+    }
+}
