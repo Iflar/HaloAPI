0018e9d12915caae68474d8a4f0bcf834680fdfb
Nathan Foreman <nathanforeman746@gmail.com>
2021-12-09 20:02:53 -0600

    Models Layer added

---
+++ HaloAPI.Models/FactionModels/FactionListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaloAPI.Models.FactionModels
+{
+    public class FactionListItem
+    {
+        public int Id { get; set; }
+        public string FactionName { get; set; }
+        public int YearActive { get; set; }
+        public string Motto { get; set; }
+        public string Engagements { get; set; }
+    }
+}
