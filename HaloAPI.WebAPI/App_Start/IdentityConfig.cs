175aef8d55aec96999752d0e058292307daf8ce9
Peter-Nikolov1 <nikolov_peter@yahoo.com>
2021-12-15 18:39:20 -0500

    Redid Species Service

--- HaloAPI.WebAPI/App_Start/IdentityConfig.cs
+++ HaloAPI.WebAPI/App_Start/IdentityConfig.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using HaloAPI.WebAPI.Models;
+using HaloAPI.Data;
 
 namespace HaloAPI.WebAPI
 {
