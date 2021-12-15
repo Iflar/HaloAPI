1851b8d3b5ef5b0fdaadbec6d4daf8de1758ba11
iflar <classicsience@gmail.com>
2021-12-15 18:17:34 -0500

    update?

--- HaloAPI.Services/CharacterService.cs
+++ HaloAPI.Services/CharacterService.cs
@@ -41,10 +41,6 @@ namespace HaloAPI.Services
                         YearActive = entity.YearActive
                     };
             }
-<<<<<<< HEAD
-
-=======
->>>>>>> 3c1ad50e1d0229477ee3fa71e9899823163bf622
         }
 
         //POST
