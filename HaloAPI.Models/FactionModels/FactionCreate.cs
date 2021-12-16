e2986cf6bbb858661d8c7bf3a0d01cad5008fd38
Nathan Foreman <nathanforeman746@gmail.com>
2021-12-15 21:07:27 -0600

    changed int to string

--- HaloAPI.Models/FactionModels/FactionCreate.cs
+++ HaloAPI.Models/FactionModels/FactionCreate.cs
@@ -9,19 +9,12 @@ namespace HaloAPI.Models.FactionModels
 {
     public class FactionCreate
     {
-        [Required]
-        [MinLength(4, ErrorMessage = "Please enter at least 4 characters.")]
-        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
         public string FactionName { get; set; }
 
-        [MaxLength(100)]
         public string Motto { get; set; }
 
-        public int YearActive { get; set;}
+        public string YearActive { get; set;}
 
-        [Required]
-        [MinLength(5000, ErrorMessage = "Please enter more characters.")]
-        [MaxLength(10000, ErrorMessage = "Too many characters in this field.")]
         public string Engagements { get; set; }
 
 
