85d26555745e0cc8dc94f1f57c4cac8c2f43ed69
iflar <classicsience@gmail.com>
2021-12-15 19:59:00 -0500

    added virtual properties in character class

--- HaloAPI.WebAPI/Providers/ApplicationOAuthProvider.cs
+++ HaloAPI.WebAPI/Providers/ApplicationOAuthProvider.cs
@@ -10,6 +10,7 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
 using HaloAPI.WebAPI.Models;
+using HaloAPI.Data;
 
 namespace HaloAPI.WebAPI.Providers
 {
